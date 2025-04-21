57c338d6b28643b66770b4179d10e5a9f4cdee72
venkylunavth <venkateshwarlulunavath8@gmail.com>
2025-04-21 19:22:15 +0530

    initial commit

---
+++ src/main/java/com/hibernate/txmgnt/TrasactionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.txmgnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasactionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasactionManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate/txmgnt/TrasactionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate.txmgnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasactionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
